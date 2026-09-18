0cb0f2bfc58676d5136adadacb118967d91107c7
JAYRAJASHOKKAMLIYA <jayrajkamliya999@gmail.com>
2026-09-18 17:38:07 +0530

    Print statement for test

--- src/main/java/com/jayraj/MusicConvertor/MusicConvertorApplication.java
+++ src/main/java/com/jayraj/MusicConvertor/MusicConvertorApplication.java
@@ -8,6 +8,7 @@ public class MusicConvertorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusicConvertorApplication.class, args);
+		System.out.println("Running");
 	}
 
 }
