487e49dd637afcbc81936f7450cb5a935fb15dff
JAYRAJASHOKKAMLIYA <jayrajkamliya999@gmail.com>
2026-09-18 17:23:25 +0530

    Create a new Project

---
+++ src/main/java/com/jayraj/MusicConvertor/MusicConvertorApplication.java
@@ -0,0 +1,13 @@
+package com.jayraj.MusicConvertor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicConvertorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicConvertorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jayraj/MusicConvertor/MusicConvertorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayraj.MusicConvertor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicConvertorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
