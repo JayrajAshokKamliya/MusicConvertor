eefb5522d35391537246a49d81da3b3a06c1a0af
JAYRAJASHOKKAMLIYA <jayrajkamliya999@gmail.com>
2026-09-19 12:34:02 +0530

    Juat Practice

--- src/main/java/com/jayraj/MusicConvertor/MusicConvertorApplication.java
+++ src/main/java/com/jayraj/MusicConvertor/MusicConvertorApplication.java
@@ -3,12 +3,15 @@ package com.jayraj.MusicConvertor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class MusicConvertorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusicConvertorApplication.class, args);
 		System.out.println("Running");
+		System.out.println("Testing");
 	}
 
 }
